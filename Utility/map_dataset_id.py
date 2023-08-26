7d2af985d93b4ca6345093289fff22b49bc44643
Lyonel Behringer <lyonel.behringer@hotmail.de>
2023-08-26 23:02:11 +0200

    clarify dataset names

--- Utility/map_dataset_id.py
+++ Utility/map_dataset_id.py
@@ -1,6 +1,6 @@
 import os
 import torch
-from Utility.storage_config import PREPROCESSING_DIR    
+from Utility.storage_config import PREPROCESSING_DIR
 
 
 def map_dataset_to_dataset_id(corpus_dir):
@@ -13,8 +13,8 @@ def map_dataset_to_dataset_id(corpus_dir):
     # if parent dirs are the same as the default preprocessing dir, only use the basename as dataset name
     if os.path.dirname(corpus_dir) == PREPROCESSING_DIR.rstrip("/"):
         corpus_dir = os.path.basename(corpus_dir)
-        
-    if corpus_dir == "VCTK_single_uk_with_us_g2p_mix_emb":
+
+    if corpus_dir == "VCTK_uk_p228_with_us_g2p_mix_emb":
         return torch.LongTensor([0])
     elif corpus_dir == "VCTK_uk_p229_with_us_g2p_mix_emb":
         return torch.LongTensor([1])
@@ -22,7 +22,7 @@ def map_dataset_to_dataset_id(corpus_dir):
         return torch.LongTensor([2])
     elif corpus_dir == "VCTK_us_p300_with_us_g2p_mix_emb":
         return torch.LongTensor([3])
-    elif corpus_dir == "VCTK_single_uk_with_uk_g2p_mix_emb":
+    elif corpus_dir == "VCTK_uk_p228_with_uk_g2p_mix_emb":
         return torch.LongTensor([4])
     elif corpus_dir == "VCTK_uk_p229_with_uk_g2p_mix_emb":
         return torch.LongTensor([5])
@@ -30,18 +30,18 @@ def map_dataset_to_dataset_id(corpus_dir):
         return torch.LongTensor([6])
     elif corpus_dir == "VCTK_us_p300_with_uk_g2p_mix_emb":
         return torch.LongTensor([7])
-    elif corpus_dir == "VCTK_single_uk_with_us_g2p":
+    elif corpus_dir == "VCTK_uk_p228_with_us_g2p":
         return torch.LongTensor([8])
     elif corpus_dir == "VCTK_uk_p229_with_us_g2p":
-        return torch.LongTensor([9])    
+        return torch.LongTensor([9])
     elif corpus_dir == "VCTK_us_p299_with_us_g2p":
         return torch.LongTensor([10])
     elif corpus_dir == "VCTK_us_p300_with_us_g2p":
-        return torch.LongTensor([11])    
-    elif corpus_dir == "VCTK_single_uk_with_uk_g2p":
+        return torch.LongTensor([11])
+    elif corpus_dir == "VCTK_uk_p228_with_uk_g2p":
         return torch.LongTensor([12])
     elif corpus_dir == "VCTK_uk_p229_with_uk_g2p":
-        return torch.LongTensor([13])    
+        return torch.LongTensor([13])
     elif corpus_dir == "VCTK_us_p299_with_uk_g2p":
         return torch.LongTensor([14])
     elif corpus_dir == "VCTK_us_p300_with_uk_g2p":
